9188c9d1e2031916e03c3202ebf9e319d789d335
capjie <13777857953@163.com>
2021-04-19 11:11:36 +0800

    push to

--- upload.php
+++ upload.php
@@ -20,6 +20,7 @@
             // echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
             // echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
             // echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"];
+            // ???
             if (file_exists("upload/" . $_FILES["file"]["name"]))
             {
                 echo $_FILES["file"]["name"] . " 文件已经存在。 ";
