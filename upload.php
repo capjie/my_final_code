fd1b6c8f1a3f123569fe066187d679caf6d6f2e6
capjie <13777857953@163.com>
2021-04-19 11:37:51 +0800

    push to

--- upload.php
+++ upload.php
@@ -20,16 +20,28 @@
             // echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
             // echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
             // echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"];
-            // ???
-            if (file_exists("upload/" . $_FILES["file"]["name"]))
+            $filepath = "upload/";
+            $filename = md5($_FILES["file"]["name"]);
+            if (file_exists($filepath . $filename . $extenion))
             {
-                echo $_FILES["file"]["name"] . " 文件已经存在。 ";
+                echo "文件已经存在。 ";
             }
             else
             {
                 // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-                move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-                echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
+                move_uploaded_file($_FILES["file"]["tmp_name"], $filepath . $filename . $extenion);
+                // echo "文件存储在: " . "upload/" . $filename . $extenion;
+                // 数据库参数,将文件信息和用户对应上  
+                $servername = "127.0.0.1";
+                $username = "root";
+                $password = "root";
+                $dbname = "bs";
+                $con = mysqli_connect($servername, $username, $password, $dbname);
+                if(!$con){
+                    echo "error";
+                }else{
+                    // $up_sql = sprintf("alert")
+                }
             }
         }
     }else{
